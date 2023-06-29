e28ab1cc9d851298afd6ec3c3dff981821739cf4
miguelfernandezhernandez <138075159+miguelfernandezhernandez@users.noreply.github.com>
2023-06-29 09:40:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var x = "miguel";
+x = 21;
+var firstname ="miguel";
+var lastname ="fernanedz";
+var numberOfSiblings = 2
+var hasPets = true;
+
+
+console.log(firstname)
+console.log(lastname)
+console.log(numberOfSiblings)
+console.log(hasPets)
+
+
+
+if(hasPets === true){
+console.log("Cats are better than dogs")
+
+
+}
+
+var button = document.getElementById("button");
+var h1 = document.getElementById("the reds");
+var boby = documnt.body;
+
+
+
+button.addEventListener(click), function(){
+    h1.innerHTML = " the reds are going to win!";
+    
+    "
+
+
